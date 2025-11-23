3a046516e65bf06a313d7dbf8e5cd061dc94e3ab
Neal Fachan <neal@fachan.com>
2025-11-23 20:01:45 +0000

    Add basic clap argument parsing with --help support.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
+use clap::Parser;
 use color_eyre::eyre::Result;
+
+#[derive(Parser)]
+#[command(name = "docker-tui")]
+#[command(about = "A Terminal User Interface for Docker")]
+struct Args {}
 
 fn main() -> Result<()> {
     color_eyre::install()?;
+    let _ = Args::parse();
     docker_tui::main()
 }
