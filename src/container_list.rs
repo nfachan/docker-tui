8959fd6378bb8c3b57d18e976ae918610d6e43fd
Neal Fachan <neal@fachan.com>
2025-11-27 23:33:23 +0000

    Move App to container_list::ContainerList.

---
+++ src/container_list.rs
@@ -0,0 +1,93 @@
+use crate::{docker::Container, viewport::Viewport};
+use crossterm::event::{KeyCode, KeyEvent, KeyModifiers};
+use ratatui::{
+    prelude::*,
+    widgets::{Block, List, ListItem, Paragraph},
+};
+use std::{cmp, ops::ControlFlow};
+
+#[derive(Default)]
+pub struct ContainerList {
+    containers: Vec<Container>,
+    viewport: Viewport,
+}
+
+impl ContainerList {
+    fn block() -> Block<'static> {
+        Block::bordered().title("Containers")
+    }
+
+    pub fn handle_key_event(&mut self, key_event: KeyEvent) -> ControlFlow<()> {
+        match (key_event.code, key_event.modifiers) {
+            (KeyCode::Char('q') | KeyCode::Esc, _) => {
+                return ControlFlow::Break(());
+            }
+            (KeyCode::Char('k') | KeyCode::Up, _) => {
+                self.viewport.move_selection_up_one_line();
+            }
+            (KeyCode::Char('j') | KeyCode::Down, _) => {
+                self.viewport.move_selection_down_one_line();
+            }
+            (KeyCode::Char('y'), KeyModifiers::CONTROL) => {
+                self.viewport.scroll_up_n_lines(1);
+            }
+            (KeyCode::Char('u'), KeyModifiers::CONTROL) => {
+                self.viewport
+                    .scroll_up_n_lines(cmp::max(1, self.viewport.height() / 2));
+            }
+            (KeyCode::Char('b'), KeyModifiers::CONTROL) => {
+                self.viewport
+                    .scroll_up_n_lines(cmp::max(1, self.viewport.height().saturating_sub(2)));
+            }
+            (KeyCode::Char('e'), KeyModifiers::CONTROL) => {
+                self.viewport.scroll_down_n_lines(1);
+            }
+            (KeyCode::Char('d'), KeyModifiers::CONTROL) => {
+                self.viewport
+                    .scroll_down_n_lines(cmp::max(1, self.viewport.height() / 2));
+            }
+            (KeyCode::Char('f'), KeyModifiers::CONTROL) => {
+                self.viewport
+                    .scroll_down_n_lines(cmp::max(1, self.viewport.height().saturating_sub(2)));
+            }
+            _ => {}
+        }
+        ControlFlow::Continue(())
+    }
+
+    pub fn handle_resize(&mut self, height: u16) {
+        let viewport_height = Self::block().inner(Rect::new(0, 0, 1, height)).height;
+        self.viewport.change_viewport_height(viewport_height.into());
+    }
+
+    pub fn handle_containers(&mut self, containers: Vec<Container>) {
+        self.containers = containers;
+        self.viewport.change_num_containers(self.containers.len());
+    }
+}
+
+impl Widget for &mut ContainerList {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let inner_area = ContainerList::block().inner(area);
+        if self.containers.is_empty() {
+            Paragraph::new("no containers")
+                .block(ContainerList::block())
+                .render(area, buf);
+        } else {
+            // We may not always get the resize event before some other event that causes a redraw.
+            if self.viewport.height() != usize::from(inner_area.height) {
+                self.handle_resize(area.height);
+            }
+
+            let items = self.viewport.select_for_render(
+                &self.containers[..],
+                |container| ListItem::new(format!("  {} - {}", container.name, container.status)),
+                |container| {
+                    ListItem::new(format!("> {} - {}", container.name, container.status))
+                        .add_modifier(Modifier::REVERSED)
+                },
+            );
+            Widget::render(List::new(items).block(ContainerList::block()), area, buf);
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,18 @@
 use bollard::Docker;
 use color_eyre::eyre::{Report, Result};
+use container_list::ContainerList;
 use crossterm::{
     cursor,
-    event::{KeyCode, KeyEvent, KeyEventKind, KeyModifiers},
+    event::KeyEventKind,
     execute,
     terminal::{self, EnterAlternateScreen, LeaveAlternateScreen},
 };
 use docker::Container;
-use ratatui::{
-    prelude::*,
-    widgets::{Block, List, ListItem, Paragraph},
-};
-use std::{cmp, io, ops::ControlFlow, panic, thread};
+use ratatui::prelude::*;
+use std::{io, ops::ControlFlow, panic, thread};
 use tokio::sync::mpsc;
-use viewport::Viewport;
 
+mod container_list;
 mod docker;
 mod input;
 mod viewport;
@@ -23,92 +21,6 @@ mod viewport;
 pub enum Event {
     Input(Result<input::Event>),
     Docker(Result<Vec<Container>>),
-}
-
-#[derive(Default)]
-struct App {
-    containers: Vec<Container>,
-    viewport: Viewport,
-}
-
-impl App {
-    fn block() -> Block<'static> {
-        Block::bordered().title("Containers")
-    }
-
-    fn handle_key_event(&mut self, key_event: KeyEvent) -> ControlFlow<()> {
-        match (key_event.code, key_event.modifiers) {
-            (KeyCode::Char('q') | KeyCode::Esc, _) => {
-                return ControlFlow::Break(());
-            }
-            (KeyCode::Char('k') | KeyCode::Up, _) => {
-                self.viewport.move_selection_up_one_line();
-            }
-            (KeyCode::Char('j') | KeyCode::Down, _) => {
-                self.viewport.move_selection_down_one_line();
-            }
-            (KeyCode::Char('y'), KeyModifiers::CONTROL) => {
-                self.viewport.scroll_up_n_lines(1);
-            }
-            (KeyCode::Char('u'), KeyModifiers::CONTROL) => {
-                self.viewport
-                    .scroll_up_n_lines(cmp::max(1, self.viewport.height() / 2));
-            }
-            (KeyCode::Char('b'), KeyModifiers::CONTROL) => {
-                self.viewport
-                    .scroll_up_n_lines(cmp::max(1, self.viewport.height().saturating_sub(2)));
-            }
-            (KeyCode::Char('e'), KeyModifiers::CONTROL) => {
-                self.viewport.scroll_down_n_lines(1);
-            }
-            (KeyCode::Char('d'), KeyModifiers::CONTROL) => {
-                self.viewport
-                    .scroll_down_n_lines(cmp::max(1, self.viewport.height() / 2));
-            }
-            (KeyCode::Char('f'), KeyModifiers::CONTROL) => {
-                self.viewport
-                    .scroll_down_n_lines(cmp::max(1, self.viewport.height().saturating_sub(2)));
-            }
-            _ => {}
-        }
-        ControlFlow::Continue(())
-    }
-
-    fn handle_resize(&mut self, height: u16) {
-        let viewport_height = Self::block().inner(Rect::new(0, 0, 1, height)).height;
-        self.viewport.change_viewport_height(viewport_height.into());
-    }
-
-    fn handle_containers(&mut self, containers: Vec<Container>) {
-        self.containers = containers;
-        self.viewport.change_num_containers(self.containers.len());
-    }
-}
-
-impl Widget for &mut App {
-    fn render(self, area: Rect, buf: &mut Buffer) {
-        let inner_area = App::block().inner(area);
-        if self.containers.is_empty() {
-            Paragraph::new("no containers")
-                .block(App::block())
-                .render(area, buf);
-        } else {
-            // We may not always get the resize event before some other event that causes a redraw.
-            if self.viewport.height() != usize::from(inner_area.height) {
-                self.handle_resize(area.height);
-            }
-
-            let items = self.viewport.select_for_render(
-                &self.containers[..],
-                |container| ListItem::new(format!("  {} - {}", container.name, container.status)),
-                |container| {
-                    ListItem::new(format!("> {} - {}", container.name, container.status))
-                        .add_modifier(Modifier::REVERSED)
-                },
-            );
-            Widget::render(List::new(items).block(App::block()), area, buf);
-        }
-    }
 }
 
 fn main_loop(docker: Docker, mut terminal: Terminal<impl Backend>) -> Result<()> {
@@ -120,25 +32,25 @@ fn main_loop(docker: Docker, mut terminal: Terminal<impl Backend>) -> Result<()>
 
     // Spawn input event thread.
     thread::spawn(move || input::main(sender));
-    let mut app = App::default();
+    let mut container_list = ContainerList::default();
 
-    terminal.draw(|frame| frame.render_widget(&mut app, frame.area()))?;
+    terminal.draw(|frame| frame.render_widget(&mut container_list, frame.area()))?;
     while let Some(event) = receiver.blocking_recv() {
         match event {
             Event::Input(Ok(input::Event::Key(key))) if key.kind == KeyEventKind::Press => {
-                if let ControlFlow::Break(_) = app.handle_key_event(key) {
+                if let ControlFlow::Break(_) = container_list.handle_key_event(key) {
                     break;
                 }
             }
             Event::Input(Ok(_)) => {}
             Event::Docker(Ok(containers)) => {
-                app.handle_containers(containers);
+                container_list.handle_containers(containers);
             }
             Event::Input(Err(err)) | Event::Docker(Err(err)) => {
                 return Err(err);
             }
         }
-        terminal.draw(|frame| frame.render_widget(&mut app, frame.area()))?;
+        terminal.draw(|frame| frame.render_widget(&mut container_list, frame.area()))?;
     }
     Ok(())
 }
