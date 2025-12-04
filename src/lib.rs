cd27f14b6e97184a6ae0ef7ac671e98e3160426b
Neal Fachan <neal@fachan.com>
2025-12-04 13:44:44 -0800

    Extract a new App struct.
    This will give us a place to hold some global state.

--- src/lib.rs
+++ src/lib.rs
@@ -22,72 +22,79 @@ pub enum Event {
     FromDocker(docker::MessageOut),
 }
 
-fn handle_container_list_events(
-    messages: impl IntoIterator<Item = container_list::MessageOut>,
-    docker_sender: &mut mpsc::UnboundedSender<docker::MessageIn>,
-) -> Result<ControlFlow<()>> {
-    for message in messages {
-        match message {
-            container_list::MessageOut::Exit => {
-                return Ok(ControlFlow::Break(()));
-            }
-            container_list::MessageOut::ToDocker(docker_message) => {
-                docker_sender.send(docker_message)?
+struct App;
+
+impl App {
+    fn handle_container_list_events(
+        &mut self,
+        messages: impl IntoIterator<Item = container_list::MessageOut>,
+        docker_sender: &mut mpsc::UnboundedSender<docker::MessageIn>,
+    ) -> Result<ControlFlow<()>> {
+        for message in messages {
+            match message {
+                container_list::MessageOut::Exit => {
+                    return Ok(ControlFlow::Break(()));
+                }
+                container_list::MessageOut::ToDocker(docker_message) => {
+                    docker_sender.send(docker_message)?
+                }
             }
         }
+        Ok(ControlFlow::Continue(()))
     }
-    Ok(ControlFlow::Continue(()))
-}
 
-fn main_loop(docker: Docker, mut terminal: Terminal<impl Backend>) -> Result<()> {
-    const CHANNEL_SLOTS: usize = 10;
-    let (mut docker_sender, docker_receiver) = mpsc::unbounded_channel();
-    let (sender, mut receiver) = mpsc::channel(CHANNEL_SLOTS);
+    fn main_loop(&mut self, docker: Docker, mut terminal: Terminal<impl Backend>) -> Result<()> {
+        const CHANNEL_SLOTS: usize = 10;
+        let (mut docker_sender, docker_receiver) = mpsc::unbounded_channel();
+        let (sender, mut receiver) = mpsc::channel(CHANNEL_SLOTS);
 
-    // Spawn the docker thread.
-    let sender_clone = sender.clone();
-    thread::spawn(move || docker::main(docker, docker_receiver, sender_clone, Event::FromDocker));
+        // Spawn the docker thread.
+        let sender_clone = sender.clone();
+        thread::spawn(move || {
+            docker::main(docker, docker_receiver, sender_clone, Event::FromDocker)
+        });
 
-    // Spawn input event thread.
-    thread::spawn(move || input::main(sender));
-    let mut container_list = ContainerList::default();
+        // Spawn input event thread.
+        thread::spawn(move || input::main(sender));
+        let mut container_list = ContainerList::default();
 
-    // Send an initial message to the docker thread.
-    docker_sender.send(docker::MessageIn::GetContainers)?;
+        // Send an initial message to the docker thread.
+        docker_sender.send(docker::MessageIn::GetContainers)?;
 
-    terminal.draw(|frame| frame.render_widget(&mut container_list, frame.area()))?;
-    while let Some(event) = receiver.blocking_recv() {
-        let mut messages_out = vec![];
-        match event {
-            Event::Input(Ok(input::Event::Key(event))) => {
-                messages_out.extend(container_list.handle_key_event(event)?);
-            }
-            Event::Input(Ok(input::Event::Mouse(event))) => {
-                container_list.handle_mouse_event(event);
-            }
-            Event::Input(Ok(input::Event::Resize(columns, rows))) => {
-                container_list.handle_resize(Rect::new(0, 0, columns, rows));
-            }
-            Event::Input(Ok(
-                input::Event::FocusGained | input::Event::FocusLost | input::Event::Paste(_),
-            )) => {
-                continue;
-            }
-            Event::Input(Err(err)) => {
-                return Err(err);
-            }
-            Event::FromDocker(message) => {
-                container_list.handle_docker_response(message)?;
-            }
-        }
-        if let ControlFlow::Break(_) =
-            handle_container_list_events(messages_out, &mut docker_sender)?
-        {
-            return Ok(());
-        }
         terminal.draw(|frame| frame.render_widget(&mut container_list, frame.area()))?;
+        while let Some(event) = receiver.blocking_recv() {
+            let mut messages_out = vec![];
+            match event {
+                Event::Input(Ok(input::Event::Key(event))) => {
+                    messages_out.extend(container_list.handle_key_event(event)?);
+                }
+                Event::Input(Ok(input::Event::Mouse(event))) => {
+                    container_list.handle_mouse_event(event);
+                }
+                Event::Input(Ok(input::Event::Resize(columns, rows))) => {
+                    container_list.handle_resize(Rect::new(0, 0, columns, rows));
+                }
+                Event::Input(Ok(
+                    input::Event::FocusGained | input::Event::FocusLost | input::Event::Paste(_),
+                )) => {
+                    continue;
+                }
+                Event::Input(Err(err)) => {
+                    return Err(err);
+                }
+                Event::FromDocker(message) => {
+                    container_list.handle_docker_response(message)?;
+                }
+            }
+            if let ControlFlow::Break(_) =
+                self.handle_container_list_events(messages_out, &mut docker_sender)?
+            {
+                return Ok(());
+            }
+            terminal.draw(|frame| frame.render_widget(&mut container_list, frame.area()))?;
+        }
+        Ok(())
     }
-    Ok(())
 }
 
 fn set_panic_hook() {
@@ -106,7 +113,7 @@ fn main_start_up(docker: Docker) -> Result<()> {
     execute!(stdout, cursor::Hide)?;
     execute!(stdout, event::EnableMouseCapture)?;
 
-    main_loop(docker, Terminal::new(CrosstermBackend::new(stdout))?)
+    App.main_loop(docker, Terminal::new(CrosstermBackend::new(stdout))?)
 }
 
 fn main_clean_up() -> Result<()> {
